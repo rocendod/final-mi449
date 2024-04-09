7ad63bec4f591cbf553b5726325b7f29509feed2
David Rocendo <rocendod@msu.edu>
2024-04-08 21:18:04 -0400

    Creating filter component and fetching api

--- src/App.js
+++ src/App.js
@@ -3,68 +3,73 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 
+
 function PokeCard() {
-
-  const [getPoke, setPoke] = useState([]);
-
-  async function Pokemon (){
-    useEffect(() => {
-      fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setPoke(data);
-        });
-    })
-  }
+  const [poke, setPoke] = useState([]);
+  
+  useEffect(() => {
+    async function Pokemon() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+      const data = await response.json();
+      console.log(data);
+      setPoke(data.results); // Assuming the data structure is { results: [...] }
+    }
+    Pokemon();
+  }, []);
 
   return (
     <>
-    {getPoke.map((pokemon) => (
-      <>
-      <h2>This Pokemon is:</h2>
-      <h3>{pokemon.name}</h3>
-      <a>{pokemon.url}</a>
-      </>
-    ))}
+    <h2>This Pokemon is:</h2>
+      {poke.map((pokemon, index) => (
+        <div key={index}>
+          <h3>{pokemon.name}</h3>
+          <a href={pokemon.url}>{pokemon.url}</a>
+        </div>
+      ))}
     </>
   )
-
 
 }
 
 function TypeFilter () {
-  <>
-  </>
+
+  const [types, setType] = useState([]);
+  useEffect(() => {
+    async function Types() {
+      const response = await fetch('https://pokeapi.co/api/v2/type');
+      const data = await response.json();
+      console.log(data);
+      setType(data.results); // Assuming the data structure is { results: [...] }
+    }
+    Types();
+  }, []);
+
+  return (
+    <>
+    <h3>Type:</h3>
+    {types.map((type) =>
+      <h3 class="bold">{type.name}</h3>
+      )}
+    </>
+  )
+
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1 className="bg-gray-100"> Pokedex </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Davi!
-        </a>
-
-        <h1>PokeDex</h1>
-        <PokeCard />
-
       </header>
+      <body className="App-body">
+      <TypeFilter />
+        <PokeCard />
+      </body>
     </div>
   );
 }
 
 export default App;
 
-// Satus 200 Orgin Error - is a secuirty error, server is concern that you are fetching third party data - Sever of third party could be miss configure - know as a cross domain error - -can disable cross-orgin restriction but don't leave it on. 
+// Status 200 Orgin Error - is a secuirty error, server is concern that you are fetching third party data - Sever of third party could be miss configure - know as a cross domain error - -can disable cross-orgin restriction but don't leave it on. 
